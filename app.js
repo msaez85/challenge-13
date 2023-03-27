4bce2522e0f08b4f77d52b4251064e65456615ef
msaez85 <mario.saez@sovos.com>
2023-03-26 22:55:57 -0300

    agregando segunda parte del ejercicio

--- app.js
+++ app.js
@@ -15,10 +15,17 @@ let var2 = parseInt(prompt('ingrese el segundo valor'));
 document.write('Ejercicio 2 <br/>');
 document.write('el primer numero es: ' + var1 + '<br/>');
 document.write('el segundo numero es: ' + var2 + '<br/>');
-document.write('la suma de los numeros es: '  + (var1 + var2) + '<br/>');
-document.write('la resta de los numeros es: '  + (var1 - var2) + '<br/>');
-document.write('el producto de los numeros es: '  + (var1 * var2) + '<br/>');
-document.write('la division de los numeros es: '  + (var1 / var2) + '<br/>');
+document.write('la suma de los dos primeros numeros es: '  + (var1 + var2) + '<br/>');
+document.write('la resta de los dos primeros numeros es: '  + (var1 - var2) + '<br/>');
+document.write('el producto de los dos primeros numeros es: '  + (var1 * var2) + '<br/>');
+document.write('la division de los dos primeros numeros es: '  + (var1 / var2) + '<br/>');
 document.write('el primer numero (' + var1 + ') es ' + ((var1 % 2) == 0?'par':'impar') + '<br/>');
 document.write('el segundo numero (' + var2 + ') es ' + ((var2 % 2) == 0?'par':'impar') + '<br/>');
+let var3 = parseInt(prompt('ingrese el tercer valor'));
+document.write('<br/>');
+document.write('el tercer numero es: ' + var3 + '<br/>');
+document.write('la suma de los tres numeros es: '  + (var1 + var2 + var3) + '<br/>');
+document.write('la resta de los tres numeros es: '  + (var1 - var2 - var3) + '<br/>');
+document.write('el producto de los tres numeros es: '  + (var1 * var2 * var3) + '<br/>');
+
 
